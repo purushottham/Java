f2aa2d6145ab7263e016d0cf159f53224b1d5432
Purushottham <naidu.9301@gmail.com>
2018-12-08 10:12:08 +0530

    chnages

--- SpringBatchDatabaseToCsv/src/main/java/com/websystique/springbatch/Main.java
+++ SpringBatchDatabaseToCsv/src/main/java/com/websystique/springbatch/Main.java
@@ -11,21 +11,27 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	@SuppressWarnings("resource")
-	public static void main(String areg[]){
-		
+	public static void main(String areg[]) {
+
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-batch-context.xml");
-		
+
 		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
 		Job job = (Job) context.getBean("examResultJob");
-	 
+
 		try {
 			JobExecution execution = jobLauncher.run(job, new JobParameters());
-			System.out.println("Job Exit Status : "+ execution.getStatus());
-	 
+			System.out.println("Job Exit Status : " + execution.getStatus());
+
 		} catch (JobExecutionException e) {
 			System.out.println("Job ExamResult failed");
 			e.printStackTrace();
 		}
 	}
+
+	//Useful links
+	// reading data from database using spring batch example
+	// https://howtodoinjava.com/spring-batch/csv-to-database-java-config-example/(H2DATbase)
+	// https://www.jackrutorial.com/2018/03/spring-boot-batch-read-from-mysql-database-and-write-into-a-csv-file-tutorial.html
+	// https://www.javaworld.com/article/2458888/spring-framework/open-source-java-projects-spring-batch.html
 
 }
